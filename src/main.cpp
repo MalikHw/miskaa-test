2691628ab9066558507d540a3abf12b1525b45f6
MalikHw <help.malicorporation@gmail.com>
2026-05-23 13:59:12 +0100

    forgor to convert to string

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ class $modify(PlayLayer) {
     bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
         if (!PlayLayer::init(level, useReplay, dontCreateObjects)) return false;
 
-        auto icon = cocos2d::CCSprite::create(Mod::get()->getResourcesDir() / "test.png");
+        auto icon = cocos2d::CCSprite::create(geode::utils::string::pathToString(Mod::get()->getResourcesDir() / "test.png").c_str());
         notifapi::fnotif("works", "info", 3.0f, {0,0,0}, 1.0f,
             notifapi::Position::TopRight, notifapi::Animation::Slide,
             "", 1.0f, icon);
