0196b716a63a2d551881021ae4550b6f5af31bce
MalikHw47 <123307564+MalikHw@users.noreply.github.com>
2026-05-23 13:44:56 +0100

    Enhance PlayLayer with notification feature
    Modify PlayLayer to show a notification with an icon.

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,18 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+#include <includes/notif/includes/notif.hpp>
+
+using namespace geode::prelude;
+
+class $modify(PlayLayer) {
+    bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
+        if (!PlayLayer::init(level, useReplay, dontCreateObjects)) return false;
+
+        auto icon = cocos2d::CCSprite::create(Mod::get()->getResourcesDir() / "test.png");
+        notifapi::fnotif("works", "info", 3.0f, {0,0,0}, 1.0f,
+            notifapi::Position::TopRight, notifapi::Animation::Slide,
+            "", 1.0f, icon);
+
+        return true;
+    }
+};
